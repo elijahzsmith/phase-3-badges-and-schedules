28f7e2741ae9e5a30183bc0aa927cfb423eacecc
Elijah Smith <elijahsmith.m@gmail.com>
2022-04-24 22:02:46 -0500

    commit tree a86b55d1c15000d315a4808fda209c65a9884907

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+require 'pry' # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator array
+    array.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms array
+    array.each_with_index.map  do |name, index|
+        "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    end
+end
+
+def printer(arr)
+    batch_badge_creator(arr).each do |item|
+        puts item
+    end
+    assign_rooms(arr).each do |item|
+        puts item
+    end
+end
